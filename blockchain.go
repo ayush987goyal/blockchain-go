7496466111f0b6480b0bd46ae308181af13af371
Ayush Goyal <ayush987goyal@gmail.com>
2018-09-02 10:16:29 +0530

    Add iterator

--- blockchain.go
+++ blockchain.go
@@ -16,6 +16,12 @@ type Blockchain struct {
 	db  *bolt.DB
 }
 
+// BlockchainIterator is used to iterate over blockchain blocks
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
 // AddBlock saves provided data as a block in blockchain
 func (bc *Blockchain) AddBlock(data string) {
 	var lastHash []byte
@@ -49,6 +55,34 @@ func (bc *Blockchain) AddBlock(data string) {
 
 		return nil
 	})
+}
+
+// Iterator ...
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
+}
+
+// Next returns next block starting from the tip
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
 
 // NewBlockchain creates a new blockchain with genesis block
